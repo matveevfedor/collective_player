129d12eb8ac8722dbb65e6ca932b2fc4b10e01b8
nekser <nicklebedev14@gmail.com>
2014-05-30 12:53:22 +0400

    deleted

--- MPD/collective_player/info.py
+++
@@ -1,9 +0,0 @@
-import json
-from mpd import MPDClient
-def get_info(track_pos):
-	client = MPDClient()
-	client.connect("localhost",6600)
-	response = client.playlistinfo(track_pos)
-	client.close()
-	client.disconnect()
-	return json.dumps(response)
